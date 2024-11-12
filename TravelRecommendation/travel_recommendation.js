ad5394122d642e24ebc98834799f0ab042f87137
GhettoGuru <136993411+GhettoGuru@users.noreply.github.com>
2024-11-12 14:52:57 -0600

    ye man

--- TravelRecommendation/travel_recommendation.js
+++ TravelRecommendation/travel_recommendation.js
@@ -43,7 +43,7 @@ const searchError = () => {
   result.innerHTML = `<p class="notfound">Sorry we can't find your search</p>`;
 };
 
-fetch("travelrecommendation.json")
+fetch("travel_recommendation.json")
   .then((res) => res.json())
   .then((data) => {
     const search = () => {
